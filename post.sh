bba06d6d8234e4fa4c19a9873b7b2fd7506bcf13
ireina7 <comcx.yang@gmail.com>
2024-08-21 02:30:07 +0800

    lifetime update

---
+++ post.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+date=$(date '+%Y-%m-%d')
+summary=./src/SUMMARY.md
+
+id=''
+path=journal/$date
+if [ "$2" ]; then
+    id=$2
+    path=journal/$date-$2.md
+else
+    path=journal/$date.md
+fi
+
+touch "./src/$path"
+
+# sed "/^- \[Journal\]/s/$/ $1/" ./src/SUMMARY.md
+
+IFS=''
+cp $summary "${summary}.old"
+cat "${summary}.old" | while read -r line ; do
+    echo $line
+    [[ $line == *"[Journal]"* ]] && echo "  - [$1](./$path)"
+done > $summary
+
+rm -f "${summary}.old"
